1c03bce5dce3e72f82f6f2e0ad367f9695c9d5b7
Martun Davitavyan <107114511+martungithub@users.noreply.github.com>
2022-06-21 16:08:20 +0400

    Update 24_Exercise_20_06_2022.py

--- Assingments_Practical_Work_20_06_2022/24_Exercise_20_06_2022.py
+++ Assingments_Practical_Work_20_06_2022/24_Exercise_20_06_2022.py
@@ -1,7 +1,7 @@
 numbers = []
 inputs = ''
 while inputs != '0':
-    inputs = input("Enter Name, or 0 to quit': ")
+    inputs = input("Enter Numbers, or 0 to quit': ")
     numbers.append(inputs)
 numbers = numbers[:-1]
 answer = 0
